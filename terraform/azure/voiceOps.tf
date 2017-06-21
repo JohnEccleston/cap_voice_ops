9ef19934ee69c073e2db28a6bf0232cc85edece1
James Relph <james.relph@capgemini.com>
2017-06-22 00:18:36 +0100

    Fix netmask

--- terraform/azure/voiceOps.tf
+++ terraform/azure/voiceOps.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "voiceOpsRm" {
 
 resource "azurerm_virtual_network" "voiceOpsVn" {
   name          = "voiceOpsNetwork"
-  address_space = ["172.16.11.0/23"]
+  address_space = ["172.16.0.0/16"]
   location      = "UK South"
   resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
 
