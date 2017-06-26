3c7aacd752ed35166ec6f49611ece147b1d1e671
James Relph <james.relph@capgemini.com>
2017-06-26 07:29:34 +0100

    Should have pushed these...

--- terraform/aws/voiceOpsAWS.tf
+++ terraform/aws/voiceOpsAWS.tf
@@ -10,18 +10,12 @@ variable "nodes_number" {
   default = "3"
 }
 
-variable "aws_access_key" {
-}
-
-variable "aws_secret_key" {
-}
-
 # Keep creds external
 
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "eu-west-1"
+#    access_key = "${var.aws_access_key}"
+#    secret_key = "${var.aws_secret_key}"
+#    region = "eu-west-1"
 }
 
 # Network Setup
@@ -112,7 +106,6 @@ resource "aws_route_table_association" "voiceOpsRta1c" {
 
 # Security Group Setup
 
-## TBD
 resource "aws_security_group" "voiceOpsNodesSg" {
   name        = "nodes.voiceops.capademy.com"
   vpc_id      = "${aws_vpc.voiceOpsVpc.id}"
@@ -229,112 +222,175 @@ resource "aws_security_group_rule" "node-to-master-tcp-443" {
 
 # IAM Role/Policy Setup
 
-## TBD
+resource "aws_iam_role" "voiceOpsMastersIamRole" {
+  name               = "voiceOpsMasters"
+  assume_role_policy = "${file("iams/aws_iam_role_voiceOpsMasters_policy.json")}"
+}
+
+resource "aws_iam_role" "voiceOpsNodesIamRole" {
+  name               = "voiceOpsNodes"
+  assume_role_policy = "${file("iams/aws_iam_role_voiceOpsNodes_policy.json")}"
+}
+
+resource "aws_iam_role_policy" "voiceOpsMastersIamRolePolicy" {
+  name   = "voiceOpsMasters"
+  role   = "${aws_iam_role.voiceOpsMastersIamRole.name}"
+  policy = "${file("iams/aws_iam_role_policy_voiceOpsMasters_policy.json")}"
+}
+
+resource "aws_iam_role_policy" "voiceOpsNodesIamRolePolicy" {
+  name   = "voiceOpsNodes"
+  role   = "${aws_iam_role.voiceOpsNodesIamRole.name}"
+  policy = "${file("iams/aws_iam_role_policy_voiceOpsNodes_policy.json")}"
+}
+
+resource "aws_iam_instance_profile" "voiceOpsMastersInstanceProfile" {
+  name  = "voiceOpsMasters"
+  role  = "${aws_iam_role.voiceOpsMastersIamRole.name}"
+}
+
+resource "aws_iam_instance_profile" "voiceOpsNodesInstanceProfile" {
+  name  = "voiceOpsNodes"
+  role  = "${aws_iam_role.voiceOpsNodesIamRole.name}"
+}
+
+# Key config
+
+## TBD - need to work out if we want to do this?
 
 # Start of instance creation
 # Create instances by setting LCs for instance type
 # Then ASGs with sizes
 
 # AWS Launch Configuration Setup
-# K8 Node launch config
-resource "aws_launch_configuration" "voiceOpsNode" {
-    name = "voiceOpsNode"
-    image_id = "ami-8e5743e8"
-    instance_type = "t2.large"
-    key_name= "demo"
-    security_groups = ["${aws_security_group.voiceOpsNodesSg.id}"]
-}
 
 # K8 Master launch config
 resource "aws_launch_configuration" "voiceOpsMaster" {
-    name = "voiceOpsMaster"
-    image_id = "ami-8e5743e8"
-    instance_type = "t2.medium"
-    key_name= "demo"
-    security_groups = ["${aws_security_group.voiceOpsMastersSg.id}"]
+    name                        = "voiceOpsMaster"
+    image_id                    = "ami-7d50491b"
+    instance_type               = "t2.medium"
+    key_name                    = "demo"
+    iam_instance_profile        = "${aws_iam_instance_profile.voiceOpsMastersInstanceProfile.id}"
+    security_groups             = ["${aws_security_group.voiceOpsMastersSg.id}"]
+    associate_public_ip_address = true
+    root_block_device = {
+      volume_type           = "gp2"
+      volume_size           = 20
+      delete_on_termination = true
+    }
+
+    lifecycle = {
+      create_before_destroy = true
+    }
+}
+
+# K8 Node launch config
+
+resource "aws_launch_configuration" "voiceOpsNode" {
+    name                        = "voiceOpsNode"
+    image_id                    = "ami-7d50491b"
+    instance_type               = "t2.large"
+    key_name                    = "demo"
+    iam_instance_profile        = "${aws_iam_instance_profile.voiceOpsNodesInstanceProfile.id}"
+    security_groups             = ["${aws_security_group.voiceOpsNodesSg.id}"]
+    associate_public_ip_address = true
+
+    root_block_device = {
+      volume_type           = "gp2"
+      volume_size           = 20
+      delete_on_termination = true
+    }
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 # Bastion host launch config
 resource "aws_launch_configuration" "voiceOpsBastion" {
-    name = "voiceOpsBastion"
-    image_id = "ami-8e5743e8"
-    instance_type = "t2.micro"
-    key_name= "demo"
-    security_groups = ["${aws_security_group.voiceOpsMastersSg.id}"]
+    name                        = "voiceOpsBastion"
+    image_id                    = "ami-7d50491b"
+    instance_type               = "t2.micro"
+    key_name                    = "demo"
+    security_groups             = ["${aws_security_group.voiceOpsMastersSg.id}"]
+    associate_public_ip_address = true
+
+    root_block_device = {
+      volume_type           = "gp2"
+      volume_size           = 20
+      delete_on_termination = true
+    }
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 # AWS Auto Scaling Group Setup
 
 # K8 Node ASG
 resource "aws_autoscaling_group" "voiceOpsNodeASG" {
-  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  name = "voiceOpsNodeASG"
-  max_size = 3
-  min_size = 3
-  desired_capacity = 3
-  health_check_type = "EC2"
-  force_delete = true
+  name                 = "voiceOpsNodeASG"
+  max_size             = 3
+  min_size             = 3
+  desired_capacity     = 3
+  health_check_type    = "EC2"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.voiceOpsNode.name}"
-  vpc_zone_identifier = ["${aws_subnet.voiceOpsSn1c.id}","${aws_subnet.voiceOpsSn1b.id}","${aws_subnet.voiceOpsSn1a.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.voiceOpsSn1c.id}","${aws_subnet.voiceOpsSn1b.id}","${aws_subnet.voiceOpsSn1a.id}"]
 
 }
 
 # K8 Master ASG
 resource "aws_autoscaling_group" "voiceOpsMasterASG1a" {
-  availability_zones = ["eu-west-1a"]
-  name = "voiceOpsMasterASG1a"
-  max_size = 1
-  min_size = 1
-  desired_capacity = 1
-  health_check_type = "EC2"
-  force_delete = true
+  name                 = "voiceOpsMasterASG1a"
+  max_size             = 1
+  min_size             = 1
+  desired_capacity     = 1
+  health_check_type    = "EC2"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.voiceOpsMaster.name}"
-  vpc_zone_identifier = ["${aws_subnet.voiceOpsSn1a.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.voiceOpsSn1a.id}"]
 
 }
 
 resource "aws_autoscaling_group" "voiceOpsMasterASG1b" {
-  availability_zones = ["eu-west-1b"]
-  name = "voiceOpsMasterASG1b"
-  max_size = 1
-  min_size = 1
-  desired_capacity = 1
-  health_check_type = "EC2"
-  force_delete = true
+  name                 = "voiceOpsMasterASG1b"
+  max_size             = 1
+  min_size             = 1
+  desired_capacity     = 1
+  health_check_type    = "EC2"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.voiceOpsMaster.name}"
-  vpc_zone_identifier = ["${aws_subnet.voiceOpsSn1b.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.voiceOpsSn1b.id}"]
 
 }
 
 resource "aws_autoscaling_group" "voiceOpsMasterASG1c" {
-  availability_zones = ["eu-west-1c"]
   name = "voiceOpsMasterASG1c"
-  max_size = 1
-  min_size = 1
-  desired_capacity = 1
-  health_check_type = "EC2"
-  force_delete = true
+  max_size             = 1
+  min_size             = 1
+  desired_capacity     = 1
+  health_check_type    = "EC2"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.voiceOpsMaster.name}"
-  vpc_zone_identifier = ["${aws_subnet.voiceOpsSn1c.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.voiceOpsSn1c.id}"]
 
 }
 
 
 # K8 Bastion ASG
 resource "aws_autoscaling_group" "voiceOpsBastionASG" {
-  availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  name = "voiceOpsBastionASG"
-  max_size = 1
-  min_size = 1
-  desired_capacity = 1
-  health_check_type = "EC2"
-  force_delete = true
+  name                 = "voiceOpsBastionASG"
+  max_size             = 1
+  min_size             = 1
+  desired_capacity     = 1
+  health_check_type    = "EC2"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.voiceOpsBastion.name}"
-  vpc_zone_identifier = ["${aws_subnet.voiceOpsSn1c.id}","${aws_subnet.voiceOpsSn1b.id}","${aws_subnet.voiceOpsSn1a.id}"]
+  vpc_zone_identifier  = ["${aws_subnet.voiceOpsSn1c.id}","${aws_subnet.voiceOpsSn1b.id}","${aws_subnet.voiceOpsSn1a.id}"]
 
 }
-
-
 
 
 # Maybe create some volumes for etcd?  Feels like this
