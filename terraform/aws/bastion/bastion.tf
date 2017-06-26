d5e6c3febce2f40a3aa7644b5297d256a64fabfe
James Relph <james.relph@capgemini.com>
2017-06-26 21:47:38 +0100

    Tweak things

--- terraform/aws/bastion/bastion.tf
+++ terraform/aws/bastion/bastion.tf
@@ -26,26 +26,6 @@ resource "aws_subnet" "voiceOpsBastionSn1a" {
   }
 }
 
-resource "aws_subnet" "voiceOpsBastionSn1b" {
-  vpc_id            = "${aws_vpc.voiceOpsBastionVpc.id}"
-  cidr_block        = "172.30.31.0/24"
-  availability_zone = "eu-west-1b"
-
-  tags = {
-    Name              = "Bastion"
-  }
-}
-
-resource "aws_subnet" "voiceOpsBastionSn1c" {
-  vpc_id            = "${aws_vpc.voiceOpsBastionVpc.id}"
-  cidr_block        = "172.30.32.0/24"
-  availability_zone = "eu-west-1c"
-
-  tags = {
-    Name              = "Bastion"
-  }
-}
-
 resource "aws_internet_gateway" "voiceOpsBastionIgw" {
   vpc_id = "${aws_vpc.voiceOpsBastionVpc.id}"
 
@@ -70,16 +50,6 @@ resource "aws_route" "bastionR" {
 
 resource "aws_route_table_association" "voiceOpsBastionRta1a" {
   subnet_id      = "${aws_subnet.voiceOpsBastionSn1a.id}"
-  route_table_id = "${aws_route_table.voiceOpsBastionRt.id}"
-}
-
-resource "aws_route_table_association" "voiceOpsBastionRta1b" {
-  subnet_id      = "${aws_subnet.voiceOpsBastionSn1b.id}"
-  route_table_id = "${aws_route_table.voiceOpsBastionRt.id}"
-}
-
-resource "aws_route_table_association" "voiceOpsBastionRta1c" {
-  subnet_id      = "${aws_subnet.voiceOpsBastionSn1c.id}"
   route_table_id = "${aws_route_table.voiceOpsBastionRt.id}"
 }
 
@@ -141,53 +111,31 @@ resource "aws_iam_instance_profile" "voiceOpsBastionInstanceProfile" {
 
 # AWS Launch Configuration Setup
 
-# Bastion host launch config
-resource "aws_launch_configuration" "voiceOpsBastion" {
-    name                        = "voiceOpsBastion"
-    image_id                    = "ami-7d50491b"
+resource "aws_instance" "voiceOpsBastion" {
+    ami                         = "ami-7d50491b"
     instance_type               = "t2.micro"
     key_name                    = "demo"
     security_groups             = ["${aws_security_group.voiceOpsBastionSg.id}"]
     associate_public_ip_address = true
+    subnet_id                   = "${aws_subnet.voiceOpsBastionSn1a.id}"
 
     root_block_device = {
       volume_type           = "gp2"
       volume_size           = 20
       delete_on_termination = true
     }
-
-    lifecycle = {
-      create_before_destroy = true
-    }
     
     provisioner "file" {
       source                = "files/bootstrap.sh"
-      destination           = "tmp/bootstrap.sh"
-    }
-}
-
-# AWS Auto Scaling Group Setup
-
-# K8 Bastion ASG
-resource "aws_autoscaling_group" "voiceOpsBastionASG" {
-  name                 = "voiceOpsBastionASG"
-  max_size             = 1
-  min_size             = 1
-  desired_capacity     = 1
-  health_check_type    = "EC2"
-  force_delete         = true
-  launch_configuration = "${aws_launch_configuration.voiceOpsBastion.name}"
-  vpc_zone_identifier  = ["${aws_subnet.voiceOpsBastionSn1a.id}","${aws_subnet.voiceOpsBastionSn1b.id}","${aws_subnet.voiceOpsBastionSn1c.id}"]
-
-  tag {
-    key              = "Name"
-    value            = "Bastion"
-    propagate_at_launch = true
+      destination           = "/tmp/bootstrap.sh"
     }
 
-  tag {
-    key              = "KeepRunning"
-    value            = "true"
-    propagate_at_launch = true
+    provisioner "local-exec" {
+      command = "/tmp/bootstrap.sh"
+    }
+    
+    tags = {
+      Name              = "Bastion"
+      KeepRunning       = "true"
   }
 }
