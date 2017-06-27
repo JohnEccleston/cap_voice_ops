a970411305eb5e2fc52beb4ac63a5b16bd57910b
James Relph <james.relph@capgemini.com>
2017-06-27 09:39:26 +0100

    Bootstrap updates

--- terraform/aws/bastion/files/bootstrap.sh
+++ terraform/aws/bastion/files/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-yum update
+yum -y update
+yum -y install git
 yum -y install epel-release
-yum -y install ansible
+yum -y --enablerepo=epel install ansible
+
+# Install Terraform
+cd /tmp
+wget https://releases.hashicorp.com/terraform/0.9.9/terraform_0.9.9_linux_amd64.zip
+unzip terraform_0.9.9_linux_amd64.zip
+mv terraform /usr/local/bin/
+
+su ec2-user <<'EOF'
+  cd ~
+  git clone https://github.com/JohnEccleston/cap_voice_ops.git
+EOF
