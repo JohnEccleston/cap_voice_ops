27226916992f81a2d9af814ccfcb72c9a52d3cbf
Doug Thomson <douglas.thomson@capgemini.com>
2017-06-27 21:56:26 +0100

    Updated with first set of security rules

--- terraform/azure/voiceOpsAzure.tf
+++ terraform/azure/voiceOpsAzure.tf
@@ -8,9 +8,18 @@ variable "nodes_number" {
   default = "3"
 }
 
+#variable "subscription_id" {}
+#variable "client_id" {}
+#variable "client_secret" {}
+#variable "tenant_id" {}
+
 
 provider "azurerm" {
 # Creds provided by environment variables
+#  subscription_id = "${var.subscription_id}"
+#  client_id       = "${var.client_id}"
+#  client_secret   = "${var.client_secret}"
+#  tenant_id       = "${var.tenant_id}"
 }
 
 # Create resource group
@@ -80,6 +89,7 @@ resource "azurerm_network_interface" "voiceOpsNiMasters" {
   name                = "vonim${count.index}"
   location            = "UK West"
   resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+  network_security_group_id = "${azurerm_network_security_group.voiceopsmaster_security_group.id}"
 
   ip_configuration {
     name                          = "voiceOpsIpConfigMasters"
@@ -93,6 +103,7 @@ resource "azurerm_network_interface" "voiceOpsNiNodes" {
   name                = "vonin${count.index}"
   location            = "UK West"
   resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+  network_security_group_id = "${azurerm_network_security_group.voiceopsnode_security_group.id}"
 
   ip_configuration {
     name                          = "voiceOpsIpConfigNodes"
@@ -110,9 +121,9 @@ resource "azurerm_virtual_machine" "voiceOpsMasters" {
   vm_size               = "Standard_A1_v2"
 
   storage_image_reference {
-    publisher = "credativ"
-    offer     = "Debian"
-    sku       = "9"
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.2"
     version   = "latest"
   }
 
@@ -148,9 +159,9 @@ resource "azurerm_virtual_machine" "voiceOpsNodes" {
   vm_size               = "Standard_A1_v2"
 
   storage_image_reference {
-    publisher = "credativ"
-    offer     = "Debian"
-    sku       = "9"
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "7.2"
     version   = "latest"
   }
 
@@ -176,3 +187,113 @@ resource "azurerm_virtual_machine" "voiceOpsNodes" {
     environment = "production"
   }
 }
+
+
+
+#Security Groups for the Masters
+resource "azurerm_network_security_group" "voiceopsmaster_security_group" {
+    name = "voiceopsmaster_security_group"
+    location = "UK West"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+
+    tags {
+        environment = "production"
+    }
+}
+
+resource "azurerm_network_security_rule" "all-master-to-master" {
+    name = "all-master-to-master"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "172.21.0.0/24"
+    destination_address_prefix = "172.21.0.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsmaster_security_group.name}"
+}
+
+resource "azurerm_network_security_rule" "https-external-to-master-0-0-0--0" {
+    name = "https-external-to-master"
+    priority = 110
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "443"
+    source_address_prefix = "*"
+    destination_address_prefix = "172.21.0.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsmaster_security_group.name}"
+}
+
+resource "azurerm_network_security_rule" "all-node-to-master-tcp-4194" {
+    name = "all-node-to-master-tcp-443"
+    priority = 120
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "4194"
+    source_address_prefix = "172.21.1.0/24"
+    destination_address_prefix = "172.21.0.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsmaster_security_group.name}"
+}
+
+resource "azurerm_network_security_rule" "all-node-to-master-tcp-443" {
+    name = "all-node-to-master-tcp-443"
+    priority = 130
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "443"
+    source_address_prefix = "172.21.1.0/24"
+    destination_address_prefix = "172.21.0.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsmaster_security_group.name}"
+}
+
+#Security Groups for the Nodes
+resource "azurerm_network_security_group" "voiceopsnode_security_group" {
+    name = "voiceopsnode_security_group"
+    location = "UK West"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+
+    tags {
+        environment = "production"
+    }
+}
+
+
+resource "azurerm_network_security_rule" "all-master-to-node" {
+    name = "all-master-to-node"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "172.21.0.0/24"
+    destination_address_prefix = "172.21.1.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsnode_security_group.name}"
+}
+
+resource "azurerm_network_security_rule" "all-node-to-node" {
+    name = "all-node-to-node"
+    priority = 110
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "172.21.1.0/24"
+    destination_address_prefix = "172.21.1.0/24"
+    resource_group_name = "${azurerm_resource_group.voiceOpsRm.name}"
+    network_security_group_name = "${azurerm_network_security_group.voiceopsnode_security_group.name}"
+}
+
